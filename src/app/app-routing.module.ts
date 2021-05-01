d232ffdbaf19c5cf7c00a4ef5d545cfdbe66a128
Marco Hernandez <marcohern@gmail.com>
2021-05-01 14:17:16 -0500

    added toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateBeerComponent } from './create-beer/create-beer.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'create-beer', component: CreateBeerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateBeerComponent } from './create-beer/create-beer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBeerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    
     BrowserAnimationsModule
   ],
   providers: [],
