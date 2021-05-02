600f3305112f80851f09b68b459a48993ec6fcbe
Marco Hernandez <marcohern@gmail.com>
2021-05-01 22:16:56 -0500

    fixed links and layout

--- src/app/sidenav/sidenav.module.ts
+++ src/app/sidenav/sidenav.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 import { SidenavComponent } from './sidenav.component';
 
@@ -16,6 +17,7 @@ import { SidenavComponent } from './sidenav.component';
     CommonModule,
     MatButtonModule,
     MatIconModule,
+    MatListModule,
   ],
   exports: [
     SidenavComponent
