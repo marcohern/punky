2cb75c9f2ed43cc8a70125627f200c66a7bf4e0c
Marco Hernandez <marcohern@gmail.com>
2021-05-02 14:28:56 -0500

    chart working

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -17,27 +17,29 @@ export class ChartComponent implements OnInit {
   selected: any[] = [];
   filteredOptions: Observable<any[]> = EMPTY;
 
-  single = [];
-  multi = [];
+  chartData:any[] = [];
 
   view:any[2] = [700, 400];
 
   // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Country';
-  showYAxisLabel = true;
-  yAxisLabel = 'Population';
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = false;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Attribute';
+  showYAxisLabel: boolean = true;
+  yAxisLabel: string = 'Value';
+  legendTitle: string = 'Years';
+  legendPosition: string = 'below';
 
   colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    domain: ['#cc4125', '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6d9eeb', '#6fa8dc','#8e7cc3','#c27ba0']
   };
+  schemeType: string = 'linear';
 
   constructor(private punky:PunkyapiService) {
-    Object.assign(this, this.single);
+    this.generateData();
     this.filteredOptions = this.name.valueChanges
       .pipe(
         startWith(''),
@@ -56,19 +58,51 @@ export class ChartComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onSelect(event:Event) {
-    console.log(event);
+
+  generateData() {
+    this.chartData = [
+      {name: 'ABV', value:0, series: []},
+      {name: 'IBU',  value:0,series: []},
+      //{name: 'TFG', value:0, series: []},
+      //{name: 'TOG',  value:0,series: []},
+      {name: 'EBC', value:0, series: []},
+      {name: 'SRM', value:0, series: []},
+      {name: 'PH' , value:0, series: []},
+    ];
+
+    for (let item of this.selected) {
+      console.log(item);
+      this.chartData[0].series.push({name: item.name, value: item.abv||0});
+      this.chartData[1].series.push({name: item.name, value: item.ibu||0});
+      //this.chartData[2].series.push({name: item.name, value: item.target_fg});
+      //this.chartData[3].series.push({name: item.name, value: item.target_og});
+      this.chartData[2].series.push({name: item.name, value: item.ebc||0});
+      this.chartData[3].series.push({name: item.name, value: item.srm||0});
+      this.chartData[4].series.push({name: item.name, value: item.ph||0});
+    }
+  }
+  chartItemSelected(event:Event) {
+    //console.log(event);
+  }
+
+  chartActivate(data:any): void {
+    //console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  chartDeactivate(data:any): void {
+    //console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
   itemSelected(event:MatAutocompleteSelectedEvent): void {
-    console.log("val",event.option.value);
+    //console.log("val",event.option.value);
     this.selected.push(event.option.value);
     this.name.setValue('');
     this.options = [];
+    this.generateData();
   }
 
   private _filter(value:string): Observable<any[]> {
-    console.log("_filter",value);
+    //console.log("_filter",value);
     if (value.length <= 2) return EMPTY;
     return this.punky.query({beer_name: value});
   }
