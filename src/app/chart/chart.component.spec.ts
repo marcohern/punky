755e1f6343f98e574dea275a9504622a5b4f52d1
Marco Hernandez <marcohern@gmail.com>
2021-05-02 17:17:30 -0500

    chart

--- src/app/chart/chart.component.spec.ts
+++ src/app/chart/chart.component.spec.ts
@@ -1,6 +1,14 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { PunkyapiService } from '../punkyapi/punkyapi.service';
 
 import { ChartComponent } from './chart.component';
+
+const fakeActivatedRoute = {
+  snapshot: { paramMap: { get:function(id:string) { return '1'; } } }
+} as ActivatedRoute;
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
@@ -8,7 +16,13 @@ describe('ChartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChartComponent ]
+      declarations: [ ChartComponent ],
+      imports: [HttpClientModule, MatSnackBarModule],
+      providers: [
+        {provide:ActivatedRoute, useValue: fakeActivatedRoute },
+        PunkyapiService,
+        HttpClient
+      ]
     })
     .compileComponents();
   });
