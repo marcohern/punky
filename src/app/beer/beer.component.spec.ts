977284189df25229eafa9e24b6af48e4d186c65a
Marco Hernandez <marcohern@gmail.com>
2021-05-02 17:12:07 -0500

    beer tests fixed

--- src/app/beer/beer.component.spec.ts
+++ src/app/beer/beer.component.spec.ts
@@ -1,6 +1,15 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { PunkyapiService } from '../punkyapi/punkyapi.service';
 
 import { BeerComponent } from './beer.component';
+
+const fakeActivatedRoute = {
+  snapshot: { paramMap: { get:function(id:string) { return '1'; } } }
+} as ActivatedRoute;
 
 describe('BeerComponent', () => {
   let component: BeerComponent;
@@ -8,7 +17,13 @@ describe('BeerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BeerComponent ]
+      declarations: [ BeerComponent ],
+      imports: [HttpClientModule, MatSnackBarModule],
+      providers: [
+        {provide:ActivatedRoute, useValue: fakeActivatedRoute },
+        PunkyapiService,
+        HttpClient
+      ]
     })
     .compileComponents();
   });
--- src/app/punkyapi/punkyapi.service.spec.ts
+++ src/app/punkyapi/punkyapi.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { PunkyapiModule } from './punkyapi.module';
 
 import { PunkyapiService } from './punkyapi.service';
 
@@ -6,11 +9,19 @@ describe('PunkyapiService', () => {
   let service: PunkyapiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, MatSnackBarModule],
+  });
     service = TestBed.inject(PunkyapiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get beer 1', () => {
+    service.getBeer('1').subscribe(data => {
+      expect(data.id).toBe('1');
+    });
+  })
 });
