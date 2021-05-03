0da4944236910f61da9cf77f249d488ff77db422
Marco Hernandez <marcohern@gmail.com>
2021-05-02 21:39:08 -0500

    search form more complex

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PunkyapiService } from '../punkyapi/punkyapi.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
+interface LooseObject {
+  [key: string]: any
+}
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -12,6 +15,16 @@ export class SearchComponent implements OnInit {
 
   beers: any[];
   name: string = '';
+  yeast: string = '';
+  malt: string = '';
+  hops: string = '';
+  food: string = '';
+  min_abv: string = '';
+  max_abv: string = '';
+  min_ibu: string = '';
+  max_ibu: string = '';
+  min_ebc: string = '';
+  max_ebc: string = '';
 
   constructor(private punky:PunkyapiService) { 
     this.beers = [];
@@ -25,9 +38,25 @@ export class SearchComponent implements OnInit {
     get: { return this.punky.loading; }
   };
 
+  buildQuery(): {[param: string]: string} {
+    let r:{[param: string]: string} = {};
+    if (this.name && this.name.length > 0) r.beer_name = this.name.replace(/\s/,'_');
+    if (this.yeast && this.yeast.length > 0) r.yeast = this.yeast.replace(/\s/,'_');
+    if (this.hops && this.hops.length > 0) r.hops = this.hops.replace(/\s/,'_');
+    if (this.malt && this.hops.length > 0) r.malt = this.malt.replace(/\s/,'_');
+    if (this.food && this.food.length > 0) r.food = this.food.replace(/\s/,'_');
+    if (this.min_abv && this.min_abv.length > 0) r.abv_gt = this.min_abv.toString();
+    if (this.max_abv && this.max_abv.length > 0) r.abv_lt = this.max_abv.toString();
+    if (this.min_ibu && this.min_ibu.length > 0) r.ibu_gt = this.min_ibu.toString();
+    if (this.max_ibu && this.max_ibu.length > 0) r.ibu_lt = this.max_ibu.toString();
+    if (this.min_ebc && this.min_ebc.length > 0) r.ebc_gt = this.min_ebc.toString();
+    if (this.max_ebc && this.max_ebc.length > 0) r.ebc_gt = this.max_ebc.toString();
+    return r;
+  }
+
   search(): void {
     this.beers = [];
-    this.punky.query({beer_name: this.name}).subscribe((data) => {
+    this.punky.query(this.buildQuery()).subscribe((data) => {
       this.beers = data;
     });
   }
